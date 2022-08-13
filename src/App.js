2c81269e29aab3a9f7d0f6ffa944fc31623739a2
mahdi kazemi <kazemi.dist@gmail.com>
2022-08-13 11:19:07 +0430

    feat: add sass, assets and some startes files

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./styles/base/_reset.scss";
+import styles from "./app.module.scss";
+
+function App() {
+  return (
+    <div className={`${styles.app}`}>
+      <h1>hello world</h1>
+    </div>
+  );
+}
+
+export default App;
